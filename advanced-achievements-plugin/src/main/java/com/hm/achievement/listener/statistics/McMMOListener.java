e21ec43c2497ad9cfbb436901f68e7f9922ee033
Greymagic27 <tolly67p@gmail.com>
2026-02-14 17:14:33 +0000

    Add ability for achievements based on party xp level

--- advanced-achievements-plugin/src/main/java/com/hm/achievement/listener/statistics/McMMOListener.java
+++ advanced-achievements-plugin/src/main/java/com/hm/achievement/listener/statistics/McMMOListener.java
@@ -2,6 +2,7 @@ package com.hm.achievement.listener.statistics;
 
 import com.gmail.nossr50.api.ExperienceAPI;
 import com.gmail.nossr50.events.experience.McMMOPlayerLevelUpEvent;
+import com.gmail.nossr50.events.party.McMMOPartyLevelUpEvent;
 import com.gmail.nossr50.events.skills.abilities.McMMOPlayerAbilityActivateEvent;
 import com.hm.achievement.category.MultipleAchievements;
 import com.hm.achievement.config.AchievementMap;
@@ -14,12 +15,13 @@ import java.util.logging.Logger;
 import javax.inject.Inject;
 import javax.inject.Named;
 import javax.inject.Singleton;
+import org.bukkit.Bukkit;
 import org.bukkit.configuration.file.YamlConfiguration;
 import org.bukkit.entity.Player;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.EventPriority;
 import org.bukkit.event.Listener;
-import org.jetbrains.annotations.NotNull;
+import org.jspecify.annotations.NonNull;
 
 @Singleton
 public class McMMOListener extends AbstractListener implements Listener {
@@ -32,7 +34,7 @@ public class McMMOListener extends AbstractListener implements Listener {
     }
 
     @EventHandler(priority = EventPriority.MONITOR, ignoreCancelled = true)
-    public void onSkillLevelUp(@NotNull McMMOPlayerLevelUpEvent event) {
+    public void onSkillLevelUp(@NonNull McMMOPlayerLevelUpEvent event) {
         Player player = event.getPlayer();
         String skillName = event.getSkill().name().toLowerCase(Locale.ENGLISH);
         if (!player.hasPermission(category.toChildPermName(skillName))) {
@@ -51,7 +53,7 @@ public class McMMOListener extends AbstractListener implements Listener {
     }
 
     @EventHandler(priority = EventPriority.MONITOR, ignoreCancelled = true)
-    public void onPowerLevelUp(@NotNull McMMOPlayerLevelUpEvent event) {
+    public void onPowerLevelUp(@NonNull McMMOPlayerLevelUpEvent event) {
         Player player = event.getPlayer();
         int powerLevel = ExperienceAPI.getPowerLevel(player);
         int oldPowerLevel = (int) cacheManager.getAndIncrementStatisticAmount(MultipleAchievements.MCMMO, "power", player.getUniqueId(), 0);
@@ -62,9 +64,21 @@ public class McMMOListener extends AbstractListener implements Listener {
     }
 
     @EventHandler(priority = EventPriority.MONITOR, ignoreCancelled = true)
-    public void onAbilityActivation(@NotNull McMMOPlayerAbilityActivateEvent event) {
+    public void onAbilityActivation(@NonNull McMMOPlayerAbilityActivateEvent event) {
         Player player = event.getPlayer();
         cacheManager.getAndIncrementStatisticAmount(MultipleAchievements.MCMMO, "ability", player.getUniqueId(), 0);
         updateStatisticAndAwardAchievementsIfAvailable(player, Collections.singleton("ability"), 1);
+    }
+
+    @EventHandler(priority = EventPriority.MONITOR, ignoreCancelled = true)
+    public void onPartyLevelUp(@NonNull McMMOPartyLevelUpEvent event) {
+        int levelsGained = event.getLevelsChanged();
+        if (levelsGained <= 0) return;
+        event.getParty().getMembers().keySet().forEach(uuid -> {
+            Player player = Bukkit.getPlayer(uuid);
+            if (player == null) return;
+            cacheManager.getAndIncrementStatisticAmount(MultipleAchievements.MCMMO, "party_level", uuid, levelsGained);
+            updateStatisticAndAwardAchievementsIfAvailable(player, Collections.singleton("party_level"), levelsGained);
+        });
     }
 }
