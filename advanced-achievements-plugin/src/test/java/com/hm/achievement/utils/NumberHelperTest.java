3cb885c1e66b7ea93df4b24e955ab86d7b6046f5
Greymagic27 <tolly67p@gmail.com>
2026-04-13 21:11:36 +0100

    Expand test suite

--- advanced-achievements-plugin/src/test/java/com/hm/achievement/utils/NumberHelperTest.java
+++ advanced-achievements-plugin/src/test/java/com/hm/achievement/utils/NumberHelperTest.java
@@ -8,9 +8,33 @@ class NumberHelperTest {
 
     @Test
     void shouldComputeNextMultipleOf9() {
-        assertEquals(18, NumberHelper.nextMultipleOf9(17));
+        assertEquals(9, NumberHelper.nextMultipleOf9(1));
+        assertEquals(9, NumberHelper.nextMultipleOf9(9));
+        assertEquals(18, NumberHelper.nextMultipleOf9(10));
         assertEquals(18, NumberHelper.nextMultipleOf9(18));
         assertEquals(27, NumberHelper.nextMultipleOf9(19));
+        assertEquals(27, NumberHelper.nextMultipleOf9(27));
+        assertEquals(36, NumberHelper.nextMultipleOf9(28));
+        assertEquals(36, NumberHelper.nextMultipleOf9(36));
     }
 
+    @Test
+    void shouldHandleZero() {
+        assertEquals(0, NumberHelper.nextMultipleOf9(0));
+    }
+
+    @Test
+    void shouldHandleExactMultiples() {
+        assertEquals(9, NumberHelper.nextMultipleOf9(9));
+        assertEquals(27, NumberHelper.nextMultipleOf9(27));
+        assertEquals(90, NumberHelper.nextMultipleOf9(90));
+    }
+
+    @Test
+    void shouldHandleNegativeNumbers() {
+        assertEquals(0, NumberHelper.nextMultipleOf9(-1));
+        assertEquals(0, NumberHelper.nextMultipleOf9(-8));
+        assertEquals(-9, NumberHelper.nextMultipleOf9(-9));
+        assertEquals(-9, NumberHelper.nextMultipleOf9(-10));
+    }
 }
